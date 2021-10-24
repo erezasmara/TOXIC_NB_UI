98eab7609326fafaecfec3a7f807d8a99433f102
erezasmara <erezasmra@gmail.com>
2021-10-24 11:03:12 +0300

    sent ping tp ml server

--- public/js/ping.js
+++ public/js/ping.js
@@ -1,12 +1,15 @@
 // wake up heroku 
-console.log('start weak up heroku ml server')
+console.log('start weak up heroku ml server ...')
 
-fetch('https://toxic-nb-apis.herokuapp.com/ping').then((response) =>{
+fetch('https://toxic-nb-apis.herokuapp.com/ping',{
+    method: "GET",
+    headers: {'Content-Type':'application/json'}}
+    ).then((response) =>{
     response.json().then((data) =>{
         if(data.error){
             console.log('Error , report to support')  // display on browser 
         }else{
-            console.log(data+ ': Wake heroku ml server is Wake .')  // display on browser
+            console.log(' Wake heroku ml server is Wake .')  // display on browser
 
         }
     })
